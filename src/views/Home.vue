240d5eb4a2ee22dd4ec5f3687244344bebb50f9b
Aman <rumman.djuice1@gmail.com>
2019-06-21 02:21:23 +0200

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p class="blue darken-2 white--text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias architecto dolores, eaque illo inventore iusto nam nisi optio perferendis praesentium quisquam quo reprehenderit sed sequi ullam unde ut velit. Ducimus.</p>
-    <p class="pink darken-3 white--text text--darken-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At et impedit ipsum obcaecati porro quas qui quos repudiandae velit voluptatibus! Accusantium ducimus id ipsa laudantium nemo nostrum officia quis tenetur?</p>
-    <h1 class="display-4">Massive Display</h1>
-    <h4 class="display-1">Small Display</h4>
-    <p class="headline">This is a head line</p>
-    <p class="subheading font-weight-bold">This is a subheading</p>
-    <p class="caption">This is a caption</p>
+    <h1 class="display-3">Home</h1>
+    <v-btn class="pink white--text">Click</v-btn>
+    <v-btn dark depressed color="pink">Click</v-btn>
+    <v-btn flat color="pink">Click</v-btn>
+    <v-btn small class="pink white--text">
+      <v-icon left>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn large class="pink white--text">
+      <v-icon left>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn fab small dark color="purple">
+      <v-icon>favorite</v-icon>
+    </v-btn>
   </div>
 </template>
 
