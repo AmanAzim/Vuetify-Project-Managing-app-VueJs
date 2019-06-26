7ea8cbc2fc134910c4c3bd75446b102a2b23d244
Aman <rumman.djuice1@gmail.com>
2019-06-26 23:11:05 +0200

    commit

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,29 +2,32 @@
   <div class="dashboard">
     <h1 class="subheading grey--text">Home</h1>
     <v-container class="my-5">
-      <v-layout row wrap>
-        <v-flex xs12 sm12 md6 >
-          <v-btn outline block color="teal">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md2 >
-          <v-btn outline block color="teal">2</v-btn>
-        </v-flex>
-        <v-flex xs4 md2 >
-          <v-btn outline block color="teal">2</v-btn>
-        </v-flex>
-        <v-flex xs4 md2 >
-          <v-btn outline block color="teal">2</v-btn>
-        </v-flex>
-      </v-layout>
+      <v-card  v-for="project in projects" :key="project.title">
+        <v-layout row wrap v-bind:class="`pa-3 project ${project.status}`">
 
-      <v-layout row wrap justify-center>
-        <v-flex xs4 md3>
-          <v-btn outline block color="success">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md3>
-          <v-btn outline block color="success">1</v-btn>
-        </v-flex>
-      </v-layout>
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Project Title</div>
+            <div>{{project.title}}</div>
+          </v-flex>
+
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+            <div>{{project.person}}</div>
+          </v-flex>
+
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Due by</div>
+            <div>{{project.due}}</div>
+          </v-flex>
+
+          <v-flex xs2 sm4 md2>
+            <div class="caption grey--text">Status</div>
+            <div>{{project.status}}</div>
+          </v-flex>
+
+        </v-layout>
+        <v-divider></v-divider>
+      </v-card>
     </v-container>
   </div>
 </template>
@@ -35,6 +38,30 @@
   export default {
     components: {
 
+    },
+    data(){
+      return {
+          projects:[
+            { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+            { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+            { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+            { title: 'Create a phone that doesn\'t suck', person: 'Bill Gates', due: '10th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+            { title: 'Create a community forum', person: 'Gouken', due: '20th Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+            { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'}
+          ],
+      }
     }
   }
 </script>
+
+<style scope>
+.project.complete{
+  border-left:4px solid #3cd1c2;
+}
+.project.ongoing{
+  border-left:4px solid orange;
+}
+.project.overdue{
+  border-left:4px solid tomato;
+}
+</style>
