e6ace571da9a95fbd2bffae7d740b1cfac66b240
Aman <rumman.djuice1@gmail.com>
2019-06-26 02:31:12 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app class="gray lighten-4">
     <Navbar/>
-    <v-content>
+    <v-content class="mx-4 my-8">
       <router-view></router-view>
     </v-content>
   </v-app>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="dashboard">
-    <h1 class="display-3">Home</h1>
-    <v-btn class="pink white--text">Click</v-btn>
-    <v-btn dark depressed color="pink">Click</v-btn>
-    <v-btn flat color="pink">Click</v-btn>
-    <v-btn small class="pink white--text">
-      <v-icon left>email</v-icon>
-      <span>email me</span>
-    </v-btn>
+    <h1 class="subheading grey--text">Home</h1>
+    <v-container class="my-5">
+      <v-layout row wrap>
+        <v-flex xs12 sm12 md6 >
+          <v-btn outline block color="teal">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md2 >
+          <v-btn outline block color="teal">2</v-btn>
+        </v-flex>
+        <v-flex xs4 md2 >
+          <v-btn outline block color="teal">2</v-btn>
+        </v-flex>
+        <v-flex xs4 md2 >
+          <v-btn outline block color="teal">2</v-btn>
+        </v-flex>
+      </v-layout>
 
-    <v-btn large class="pink white--text">
-      <v-icon left>email</v-icon>
-      <span>email me</span>
-    </v-btn>
-
-    <v-btn fab small dark color="purple">
-      <v-icon>favorite</v-icon>
-    </v-btn>
-    <hr>
-    <v-btn class="hidden-sm-and-down">Click 1</v-btn>
-    <v-btn class="hidden-sm-only">Click 2</v-btn>
+      <v-layout row wrap justify-center>
+        <v-flex xs4 md3>
+          <v-btn outline block color="success">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-btn outline block color="success">1</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
