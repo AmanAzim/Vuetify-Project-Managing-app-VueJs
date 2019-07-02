9a35ed1c0a0b7cc71cef943bdc49bf647465c03f
Aman <rumman.djuice1@gmail.com>
2019-07-03 00:24:53 +0200

    Expansion pannel updated

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,19 +2,27 @@
   <div class="projects">
     <h1 class="subheading grey--text">This is an projects page</h1>
 
-    <v-expansion-panel v-model="panel" expand>
-      <v-expansion-panel-content v-for="(item,i) in items" :key="i">
-        <template v-slot:header>
-          <div>Item</div>
-        </template>
-        <v-card>
-          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
-        </v-card>
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-
     <v-container class="my-5">
-      content
+
+      <v-expansion-panel>
+
+        <v-expansion-panel-content v-for="project in myProject" :key="project.title">
+
+          <template v-slot:header>
+            <div >{{project.title}}</div>
+          </template>
+
+          <v-card class="ml-2">
+            <v-card-text primary-title >
+              <div class="font-weight-bold">due by {{project.due}}</div>
+              <div>{{project.content}}</div>
+            </v-card-text>
+          </v-card>
+
+        </v-expansion-panel-content>
+
+      </v-expansion-panel>
+
     </v-container>
   </div>
 </template>
@@ -23,8 +31,19 @@
   export default {
     data(){
       return {
-        panel:'',
-        item:[]
+        projects:[
+          { title: 'Design a new website', person: 'Aman', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+          { title: 'Code up the homepage', person: 'Tansen', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+          { title: 'Design video thumbnails', person: 'Roza', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+          { title: 'Create a phone that doesn\'t suck', person: 'Bill Gates', due: '10th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+          { title: 'Create a community forum', person: 'X-man', due: '20th Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+          { title: 'Create a community forum', person: 'X-man', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'}
+        ],
+      }
+    },
+    computed:{
+      myProject(){
+        return this.projects.filter(project=>project.person==='Aman');
       }
     }
   }
