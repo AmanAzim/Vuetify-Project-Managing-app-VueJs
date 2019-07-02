ea0715cec77c6e59ca779d2f6b5d1a218ef38fc3
Aman <rumman.djuice1@gmail.com>
2019-07-02 23:44:57 +0200

    Expansion pannel added

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,8 +1,31 @@
 <template>
   <div class="projects">
     <h1 class="subheading grey--text">This is an projects page</h1>
+
+    <v-expansion-panel v-model="panel" expand>
+      <v-expansion-panel-content v-for="(item,i) in items" :key="i">
+        <template v-slot:header>
+          <div>Item</div>
+        </template>
+        <v-card>
+          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
+        </v-card>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+
     <v-container class="my-5">
       content
     </v-container>
   </div>
 </template>
+
+<script>
+  export default {
+    data(){
+      return {
+        panel:'',
+        item:[]
+      }
+    }
+  }
+</script>
